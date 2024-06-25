909e7fd4f89f8c226bd658cde05d08dffbf15ca7
HARSH KUMAWAT <115946158+harshk04@users.noreply.github.com>
2024-06-25 22:01:57 +0530

    Create admin_portal.py

---
+++ admin_portal.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def admin_portal():
+    st.markdown("<h1 style='text-align: center;'>Admin Portal</h1>", unsafe_allow_html=True)
+    st.write("Welcome to the Admin Portal. Here you can manage your application.")
+
+    # Add admin-specific functionalities here.
