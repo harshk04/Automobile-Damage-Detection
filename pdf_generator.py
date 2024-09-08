0214e6fea50074f3d1a3d36f7cb4d899bc65cce1
HARSH KUMAWAT <115946158+harshk04@users.noreply.github.com>
2024-09-08 13:51:24 +0530

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -14,7 +14,7 @@ def generate_pdf(car_details, prediction_json, total_price, price_details, image
 
     # Add title
     title_style = styles['Title']
-    elements.append(Paragraph("Car Damage Detection Results", title_style))
+    elements.append(Paragraph("Automobile Damage Inspection Report", title_style))
 
     # Add image
     elements.append(Spacer(1, 20))
@@ -27,14 +27,14 @@ def generate_pdf(car_details, prediction_json, total_price, price_details, image
     elements.append(car_info_title)
 
     car_info_data = [
-        ['Car Number', car_details['Registration']],
+        ['Registrotion No.', car_details['Registration']],
         ['Brand', car_details['Car Brand']],
-        ['Color', car_details['Colour']],
         ['Model', car_details['Model']],
-        ['Type', car_details['Type']],
+        ['Body Color', car_details['Colour']],
+        ['Body Style', car_details['Type']],
         ['Fuel Type', car_details['Fuel']],
-        ['Year of Manufacture', car_details['Year of Manufacture']],
-        ['Car Resell Value', car_details['Car Price']]
+        ['Registration Year', car_details['Year of Manufacture']],
+        ['Resell Value', car_details['Car Price']]
     ]
     car_info_table = Table(car_info_data)
     car_info_table.setStyle(TableStyle([
@@ -52,9 +52,10 @@ def generate_pdf(car_details, prediction_json, total_price, price_details, image
     damage_title = Paragraph("Damage Estimations:", styles['Heading2'])
     elements.append(damage_title)
 
-    damage_data = [['Confidence', 'Class', 'Price']]
+    damage_data = [['Damage Type', 'Damage Percent' , 'Estimated Cost']]
     for detail in price_details:
-        damage_data.append([f"{detail[0]:.2f}", detail[1], f"{detail[2]:.2f}"])
+        damage_data.append([ detail[1], f"{detail[0] * 100:.0f}%", f"{detail[2]:.2f}"])
+
     damage_table = Table(damage_data)
     damage_table.setStyle(TableStyle([
         ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
@@ -68,7 +69,7 @@ def generate_pdf(car_details, prediction_json, total_price, price_details, image
 
     # Add total price
     elements.append(Spacer(1, 20))
-    total_price_paragraph = Paragraph(f"Total estimated price: {total_price:.2f}", styles['Heading2'])
+    total_price_paragraph = Paragraph(f"Total Estimated Cost (INR): {total_price:.2f}", styles['Heading2'])
     elements.append(total_price_paragraph)
 
     doc.build(elements)
