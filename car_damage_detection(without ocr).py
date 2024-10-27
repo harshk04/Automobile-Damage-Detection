0a16914a83a6b98c81e86ee512034e6a913e6aee
HARSH KUMAWAT <115946158+harshk04@users.noreply.github.com>
2024-10-27 23:32:52 +0530

    Create car_damage_detection(without ocr).py

---
+++ car_damage_detection(without ocr).py
@@ -0,0 +1,147 @@
+import streamlit as st
+from PIL import Image
+import os
+import json
+from roboflow import Roboflow
+from utils import fetch_car_data, calculate_damage_estimation, fetch_car_brand_prices
+import time
+import pandas as pd
+from pdf_generator import generate_pdf
+
+def car_damage_detection_page():
+    st.subheader("Verify car authenticity by entering the car registration number.")
+    
+    # Initialize session state variables
+    if 'file_path' not in st.session_state:
+        st.session_state.file_path = None
+    if 'prediction_json' not in st.session_state:
+        st.session_state.prediction_json = None
+    if 'car_data_found' not in st.session_state:
+        st.session_state.car_data_found = False
+
+    # Enter car registration number
+    car_number = st.text_area("", height=10, key="car_number")
+
+    # Fetch car details based on registration number
+    if st.button("Fetch Car Details"):
+        if car_number:
+            with st.spinner("Fetching car data..."):
+                car_data = fetch_car_data(car_number)
+
+                if car_data is None:
+                    st.error(f"Car with Registration No '{car_number}' not found.")
+                    st.session_state.car_data_found = False
+                else:
+                    st.success("Car data found. You can proceed with damage detection.")
+                    st.session_state.car_data_found = True
+                    st.session_state.car_details = car_data  # Save car details for later use
+
+    # If car data is found, display it
+    if st.session_state.car_data_found:
+        st.write("Car Data:")
+
+        # Display car details in table
+        columns_order = ['Registration', 'Car Brand', 'Model', 'Colour', 'Type', 'Fuel', 'Year of Manufacture', 'Car Price']  # specify the order
+        car_data_df = pd.DataFrame([st.session_state.car_details])[columns_order]
+
+        car_data_df.rename(columns={'Registration': 'Registration No.'}, inplace=True)
+        car_data_df.rename(columns={'Car Brand': 'Brand'}, inplace=True)
+        car_data_df.rename(columns={'Colour': 'Body Color'}, inplace=True)
+        car_data_df.rename(columns={'Type': 'Body Style'}, inplace=True)
+        car_data_df.rename(columns={'Fuel': 'Fuel Type'}, inplace=True)
+        car_data_df.rename(columns={'Year of Manufacture': 'Registration Year'}, inplace=True)
+        car_data_df.rename(columns={'Car Price': 'Reselling Value (in INR)'}, inplace=True)
+        
+        st.table(car_data_df)
+        
+        st.markdown("***")
+        st.warning("Upload an image of the car to detect damages and estimate repair costs.")
+
+        # Upload an image of the car
+        image_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
+        if image_file is not None:
+            if not os.path.exists("tempDir"):
+                os.makedirs("tempDir")
+            file_path = os.path.join("tempDir", image_file.name)
+            with open(file_path, "wb") as f:
+                f.write(image_file.getbuffer())
+            st.session_state.file_path = file_path
+
+            col1, col2 = st.columns(2)
+            with col1:
+                st.image(Image.open(file_path), width=400, caption="Uploaded Image")
+
+            # Roboflow setup for damage detection
+            rf = Roboflow(api_key="LRvtIEC3Onz66K3Q2C0b")
+            project = rf.workspace("automobile-damage-detection").project("automobile-damage-detection")
+            model = project.version(1).model
+
+            confidence_threshold = 20
+            overlap_threshold = 20
+
+            # Predict damage using the uploaded image
+            with st.spinner("Detecting damage..."):
+                prediction = model.predict(file_path, confidence=confidence_threshold, overlap=overlap_threshold)
+                prediction.save("prediction.jpg")
+                st.session_state.prediction_json = prediction.json()
+
+            # Display the prediction image
+            if st.session_state.prediction_json:
+                with col2:
+                    st.image("prediction.jpg", width=400, caption="Predicted Image")
+
+                prediction_json = st.session_state.prediction_json
+
+                # Fetch the damage price data for the car's brand
+                car_brand = st.session_state.car_details.get("Car Brand")
+
+                # Ensure car_brand is a string
+                if isinstance(car_brand, str):
+                    price_mapping = fetch_car_brand_prices(car_brand)
+                else:
+                    st.error(f"Error: Expected 'Car Brand' to be a string, but got {type(car_brand)}")
+                    return
+
+                # Calculate the total price and damage details
+                total_price, price_details = calculate_damage_estimation(prediction_json, price_mapping)
+
+                # Display damage estimation results
+                st.markdown("## Damage Estimations")
+
+                for detail in price_details:
+                    st.markdown(f"""
+                        <div style="background-color: #b9b9c7; padding: 10px; border-radius: 5px; margin-bottom: 10px;">
+                            <h4 style="color: #333;">Damaged Part: <span style="color: #cc0000;">{detail[1]}</span></h4>
+                            <h4 style="color: #333;">Damage Percent: <span style="color: #0066cc;">{detail[0]*100} %</span></h4>
+                            <h4 style="color: #333;">Estimated Price: ₹ <span style="color: #009933;">{detail[2]:.2f}</span></h4>
+                        </div>
+                    """, unsafe_allow_html=True)
+
+                # Display the total price estimation
+                st.markdown(f"""
+                    <div style="background-color: #b9b9c7; padding: 20px; border-radius: 5px; margin-top: 20px;">
+                        <h3 style="color: #333; text-align: center;">Total estimated price of repair: ₹  <span style="color: #ff6600;">{total_price:.2f}</span></h3>
+                    </div>
+                """, unsafe_allow_html=True)
+                
+                st.markdown("***")
+                col1, col2, col3 = st.columns([1, 1, 1])
+
+                # Provide options to download results
+                with col2:
+                    st.download_button(
+                        label="Download prediction Results (JSON)",
+                        data=json.dumps(prediction_json),
+                        file_name="prediction_results.json",
+                        mime="application/json",
+                        key="download_button_json"
+                    )
+
+                    pdf_buffer = generate_pdf(st.session_state.car_details, prediction_json, total_price, price_details, "prediction.jpg")
+                    st.download_button(
+                        label="Download prediction Results (PDF)",
+                        data=pdf_buffer,
+                        file_name="prediction_results.pdf",
+                        mime="application/pdf",
+                        key="download_button_pdf"
+                    )
